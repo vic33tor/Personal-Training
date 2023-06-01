bebe4fd32fd720be221f94224425c4d2071689f3
Arturo1998 <amartinvaras@gmail.com>
2023-06-01 17:28:44 +0200

    clasecitas hechas menos mejoras css

--- src/views/InfoClase.vue
+++ src/views/InfoClase.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="contenedorMonitor rounded-xl shadow-md overflow-hidden">
     <div class="w-4/4">
-      <img :src="monitorActual.img" alt="Imagen del monitor" class="w-full" />
+      <img :src="claseActual.img" alt="Imagen del clase" class="w-full" />
     </div>
-    <div class="p-6 mb-6">
-      <div class="flex flex-row gap-2">
-        <h2 class="text-xl font-semibold">{{ monitorActual.name }}</h2>
-        <h2 class="text-xl font-semibold">{{ monitorActual.surnames }}</h2>
+    <div class="contenedorTitulo p-6 mb-6">
+      <div>
+        <h2 class="text-xl font-semibold">{{ claseActual.nombre }}</h2>
+        <p class="text-gray-500 mt-1">Duración: {{ claseActual.duracion }}</p>
+        <p class="text-gray-500 mt-1">
+          Hora: {{ claseActual.horaInicio }}-{{ claseActual.horaFin }}
+        </p>
+        <p class="text-gray-500 mt-1">
+          Selecciona el día que quieres asistir:
+          <select name="" id="" v-model="seleccionDia">
+            <option
+              v-for="(item, idx) in claseActual.dias"
+              :key="idx"
+              :value="item"
+            >
+              {{ item }}
+            </option>
+          </select>
+        </p>
       </div>
-      <p class="text-gray-500 mt-1">
-        Especialidad: {{ monitorActual.especialidad }}
-      </p>
-      <h1 class="mt-1">{{ monitorActual.desc }}</h1>
+      <div></div>
     </div>
+
     <div class="items-end content-end text-right">
       <button
-        @click="contrataMonitor(monitorActual.id)"
+        @click="asistirClase(claseActual)"
         class="bg-yellow-300 rounded-md px-4 py-2 hover:bg-yellow-500 w-4/12 mr-8"
       >
-        Contratar
+        Asistir
       </button>
     </div>
   </div>
@@ -27,54 +40,37 @@
 <script setup>
 import { onMounted, ref } from "vue";
 
-import { onDameReceta, onGetMonitorId, updateMonitorId } from "../API/firebase";
+import { onDameReceta, updateClasesContratadas } from "../API/firebase";
 import { useRoute } from "vue-router";
 import { useDatosStore } from "@/stores/DatosForm";
 
-const route = useRoute();
 const datos = useDatosStore();
-const monitorActual = ref({});
-const idMonitorActual = ref();
+const route = useRoute();
+const claseActual = ref([]);
+const clasesAsignadas = ref([]);
+const seleccionDia = ref("");
 
 onMounted(() => {
-  dameMonitor(route.query.id);
+  dameClase(route.query.id);
 });
 
-const dameMonitor = (id) => {
-  onDameReceta("MONITORES", (docs) => {
+const dameClase = (id) => {
+  onDameReceta("CLASES", (docs) => {
     docs.forEach((doc) => {
       if (doc.id === id) {
-        monitorActual.value = { id: doc.id, ...doc.data() };
+        claseActual.value = { id: doc.id, ...doc.data() };
       }
     });
   });
 };
 
-const dameIdMonitor = () => {
-  onGetMonitorId("USUARIOS", datos.getUsuario, (docs) => {
-    docs.forEach((doc) => {
-      idMonitorActual.value = doc.data().monitorid;
-    });
+const asistirClase = (claseActual) => {
+  updateClasesContratadas("USUARIOS", datos.getUsuario, {
+    nombre: claseActual.nombre,
+    dia: seleccionDia.value,
+    inicio: claseActual.horaInicio,
+    fin: claseActual.horaFin,
   });
-};
-
-const contrataMonitor = (monitorid) => {
-  dameIdMonitor();
-  if (idMonitorActual.value != "") {
-    if (
-      confirm(
-        "ya tiene un monitor contratado, ¿desea sustituir su monitor actual por el seleccionado actualmente?"
-      ) === true
-    ) {
-      updateMonitorId("USUARIOS", datos.getUsuario, { monitorid: monitorid });
-      alert("El contrato con su monitor ha sido actualizado");
-    } else {
-      alert("Operación cancelada");
-    }
-  } else {
-    updateMonitorId("USUARIOS", datos.getUsuario, { monitorid: monitorid });
-    alert("Monitor contratado");
-  }
 };
 </script>
 
@@ -85,7 +81,12 @@ const contrataMonitor = (monitorid) => {
   background-color: whitesmoke;
   margin: 0 auto;
   margin-top: 10px;
-  width: 40%;
+  width: 50%;
   padding-bottom: 50px;
+}
+
+.contenedorTitulo {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
